8c78b947061c24d6f8d1d30399f5be9740e329d3
Josh Pollock <jpollock412@gmail.com>
2019-07-11 10:33:54 -0400

    cleanup slides more

--- theme.js
+++ theme.js
@@ -3,12 +3,19 @@ import theme from "mdx-deck/themes";
 const regularTextSize = {
   fontSize: "1em"
 };
+console.log(theme.code);
 export default {
   ...theme,
+
   h1: {
     fontSize: "2.618em",
     color: "#a3bf61"
   },
+  pre: {
+    backgroundColor: "#3a3a3c",
+    color: "#93af51"
+  },
+  code: { color: "#4b4b4b" },
   h2: {
     fontSize: "1.68em",
     color: "black"
